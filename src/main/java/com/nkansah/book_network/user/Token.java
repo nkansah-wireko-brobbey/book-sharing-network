de1ec2f9aab0ef6d0645a87220f690e15637bd22
Nkansah <nkansahwirekobrobbey@gmail.com>
2024-09-06 01:33:23 -0500

    wip: security config setup

---
+++ src/main/java/com/nkansah/book_network/user/Token.java
@@ -0,0 +1,31 @@
+package com.nkansah.book_network.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer Id;
+
+    private String token;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+}
